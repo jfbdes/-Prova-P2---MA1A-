ddd36dd57b562ade0e1134f34c6c99ef34fff20f
jfbdes <49196387+jfbdes@users.noreply.github.com>
2019-06-18 10:36:46 -0300

    Update exc3.py

--- exc3.py
+++ exc3.py
@@ -9,27 +9,27 @@ s8=0
 s9=0
 v= open('series.csv','r')
 for line in v.readlines():
-    sep= line.split(',')
-    if sep[0]=="The Big Bang Theory":
+    s= line.split(',')
+    if s[0]=="The Big Bang Theory":
         s1= s1+1
-    elif sep[0]=="Friends":
+    elif s[0]=="Friends":
         s2= s2 + 1 
-    elif sep[0]=="Breaking Bad":
+    elif s[0]=="Breaking Bad":
         s3= s3+1
-    elif sep[0]=="Black Mirror":
+    elif s[0]=="Black Mirror":
         s4= s4+1
 n= open('series_novas.csv','r')
 for line1 in n.readlines():
-    sep1= line1.split(',')
-    if sep1[0]=="Suits":
+    sn= line1.split(',')
+    if sn[0]=="Suits":
         s5= s5+1
-    elif sep1[0]=="Fuller House":
+    elif sn[0]=="Fuller House":
         s6= s6+1
-    elif sep1[0]=="Designated Survivor":
+    elif sn[0]=="Designated Survivor":
         s7= s7+1
-    elif sep1[0]=="Lucifer":
+    elif sn[0]=="Lucifer":
         s8= s8+1
-    elif sep1[0] == "Narcos":
+    elif sn[0] == "Narcos":
         s9 = s9 +1
     
 print ('The Big Bang Theory:',s1,'episódios')
